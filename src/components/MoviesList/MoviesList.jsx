bb591b8554fe29f0c0ea48c34b6c87182418d747
Elvira Petrenko <elvirapetrenko@gmail.com>
2025-11-24 21:56:05 +0100

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,26 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query = '' }) => {
+  const normalizedQuery = query.trim().toLowerCase();
+
+  const visibleMovies = movies.filter(movie => {
+    const title = movie.title.toLowerCase().trim();
+    const description = movie.description.toLowerCase().trim();
+
+    if (!normalizedQuery) return true;
+
+    const exactMatchTitle = title === normalizedQuery;
+    const partiaMatchDescription = description.includes(normalizedQuery);
+
+    return exactMatchTitle || partiaMatchDescription;
+  });
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
